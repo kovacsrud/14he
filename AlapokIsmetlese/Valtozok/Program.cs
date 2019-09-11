90b539609426ba564488f173eacc68b6db30ef96
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-11 14:57:49 +0200

    Egész számok

--- AlapokIsmetlese/Valtozok/Program.cs
+++ AlapokIsmetlese/Valtozok/Program.cs
@@ -10,6 +10,20 @@ namespace Valtozok
     {
         static void Main(string[] args)
         {
+            //egész számok 16,32,64 bitesek
+            //előjeles
+            int a = 1;
+            //előjel nélküli
+            uint b = 126;
+
+            Int16 c = 445;
+            short d = 442;
+            long e = 224354656;
+
+            //előjel nélküli
+            UInt64 f = 55355666;
+
+            Console.ReadKey();
         }
     }
 }
--- AlapokIsmetlese/Valtozok/Valtozok.md
+++ AlapokIsmetlese/Valtozok/Valtozok.md
@@ -4,3 +4,26 @@
 A változó a memória egy része, amihez egy nevet rendelünk, és ezen a néven tudunk hivatkozni a változóra.
 
 ## Érték típusok
+
+### Egész számok
+
+**egész számok 16,32,64 bitesek**
+ -előjeles
+```c#
+   int a = 1;
+```
+
+ - előjel nélküli
+```C#
+uint b = 126;
+```
+```C#
+ Int16 c = 445;
+ short d = 442;
+ long e = 224354656;
+```
+
+ - előjel nélküli, 64 bites
+ ```c#
+ UInt64 f = 55355666;
+ ```
