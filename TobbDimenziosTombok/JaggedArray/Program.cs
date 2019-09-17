5e513875783a040dae2b2956e60363a17f25442e
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-17 15:04:52 +0200

    Furesztomb listazas

--- TobbDimenziosTombok/JaggedArray/Program.cs
+++ TobbDimenziosTombok/JaggedArray/Program.cs
@@ -16,7 +16,14 @@ namespace JaggedArray
             beagyazott[1] = new int[]{ 4, 5, 6, 7, 8 };
             beagyazott[2] = new int[] { 9, 10 };
 
-
+            for (int i = 0; i < beagyazott.Length; i++)
+            {
+                for (int j = 0; j < beagyazott[i].Length; j++)
+                {
+                    Console.Write(beagyazott[i][j]+" ");
+                }
+                Console.WriteLine();
+            }
 
 
 
