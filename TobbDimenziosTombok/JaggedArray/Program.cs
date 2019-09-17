e4a398309927b1dc4f154328a2025248676b31ed
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-17 14:27:52 +0200

    JaggedArray

---
+++ TobbDimenziosTombok/JaggedArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] beagyazott = new int[3][];
+
+            beagyazott[0] =new int[]{1,2,3 };
+            beagyazott[1] = new int[]{ 4, 5, 6, 7, 8 };
+            beagyazott[2] = new int[] { 9, 10 };
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
