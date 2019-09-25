f6d72e34f57c9ff525ba58ecc1135856900dd2f3
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-25 18:05:22 +0200

    Listák kezelése

---
+++ ObjektumAlapok/Listak/Ember.cs
@@ -0,0 +1,9 @@
+﻿namespace Listak
+{
+    internal class Ember
+    {
+        public int Eletkor { get; set; }
+        public int Magassag { get; set; }
+        public string Nev { get; set; }
+    }
+}
---
+++ ObjektumAlapok/Listak/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ember> emberek = new List<Ember>();
+
+            Ember ember = new Ember
+            {
+                Nev="Kiss Elek",
+                Magassag=178,
+                Eletkor=49
+            };
+            emberek.Add(ember);
+
+            Ember ember2 = new Ember
+            {
+                Nev = "Muhari Géza",
+                Magassag = 199,
+                Eletkor = 26
+            };
+
+            emberek.Add(ember2);
+
+            Ember ember3 = new Ember
+            {
+                Nev = "Nagyváradi Jenő",
+                Magassag = 169,
+                Eletkor = 34
+            };
+
+            emberek.Add(ember3);
+
+
+            Console.ReadKey();
+        }
+    }
+}
