4b3a52fd55912950cbae58fcf4d095ed90296015
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-01 14:46:26 +0200

    Síkidomok példányosítása

--- Interfeszek/Interfeszek/Program.cs
+++ Interfeszek/Interfeszek/Program.cs
@@ -10,6 +10,14 @@ namespace Interfeszek
     {
         static void Main(string[] args)
         {
+            Kor kor1 = new Kor(19);
+            Kor kor2 = new Kor(26.78);
+            Teglalap tegla1 = new Teglalap(16.8, 23);
+            Teglalap tegla2 = new Teglalap(34.29, 45);
+
+            Console.WriteLine($"Kör:{kor1.Kerulet()},{kor1.Terulet()}");
+            Console.WriteLine($"Téglalap:{tegla2.Kerulet()},{tegla2.Terulet()}");
+
 
 
             Console.ReadKey();
