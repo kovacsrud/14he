092490745c83e3abc668aa9bfb9eb0eb31673631
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-01 18:33:55 +0200

    Kivételek, using szemléltetés

---
+++ Kivetelek/Kivetelek/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetelek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentException();
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.WriteLine(ex.Source);
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                //Minden kivételt elkap
+                Console.WriteLine(ex.Message);
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Kivetelek/Ram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Ram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (!Console.KeyAvailable)
+            {
+                using (var bmap = new Bitmap(1280, 1024))
+                {
+                    Console.WriteLine(GC.GetTotalMemory(false));
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
