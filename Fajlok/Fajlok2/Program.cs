e74d08b18ca51bd18169373fb19806a2ae5549cf
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-01 17:40:56 +0200

    Fájl betöltés alternatívái

--- Fajlok/Fajlok2/Program.cs
+++ Fajlok/Fajlok2/Program.cs
@@ -11,6 +11,31 @@ namespace Fajlok2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var sorok = File.ReadAllLines(@"d:/rud/tesztadat_20k.txt",Encoding.Default);
+
+                //for (int i = 0; i < sorok.Length; i++)
+                //{
+                //    Console.WriteLine(sorok[i]);
+                //}
+                
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                
+            }
+
+
+            if (File.Exists(@"d:/rud/tesztadat_200k.txt"))
+            {
+                var sorok = File.ReadAllLines(@"d:/rud/tesztadat_20k.txt", Encoding.Default);
+            }else
+            {
+                Console.WriteLine("A fájl nem létezik!");
+            }
 
 
             Console.ReadKey();
