0736934dca2faf0c0a7f5b3a30cc4f6319f5f67d
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-01 14:42:20 +0200

    Teglalap osztály létrehozása

---
+++ Interfeszek/Interfeszek/Teglalap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfeszek
+{
+    class Teglalap : ISikidom
+    {
+        private double aoldal;
+        private double boldal;
+
+        public Teglalap(double aoldal, double boldal)
+        {
+            this.aoldal = aoldal;
+            this.boldal = boldal;
+        }
+
+        public double Kerulet()
+        {
+            return (aoldal + boldal) * 2;
+        }
+
+        public double Terulet()
+        {
+            return aoldal * boldal;
+        }
+    }
+}
