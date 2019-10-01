12b1e114cd5c260514f8f384ad18df4c94c53d8e
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-01 14:36:22 +0200

    Interfész létrehozás
    Interfész létrehozása, Kor sikidomot kezelő osztály létrehozása

---
+++ Interfeszek/Interfeszek/Kor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfeszek
+{
+    class Kor : ISikidom
+    {
+        private double sugar;
+
+        public Kor(double s)
+        {
+            sugar = s;
+        }
+
+        public double Kerulet()
+        {
+            return 2 * sugar * Math.PI;
+        }
+
+        public double Terulet()
+        {
+            return sugar * sugar * Math.PI;
+        }
+    }
+}
