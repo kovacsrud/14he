4d1c2fc27d2eae55f71e2ede25d572429bd499f0
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-15 18:35:30 +0200

    Elemek futási időben

---
+++ ElemekFutasiIdoben/ElemekFutasiIdoben/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElemekFutasiIdoben
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            buttonHozzaad.Click += buttonHozzaad_Click;
+        }
+
+        private void buttonHozzaad_Click(object sender, RoutedEventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Button gomb = new Button();
+                gomb.Content = $"Gomb:{i}";
+                gomb.Margin = new Thickness(5);
+                elemek.Children.Add(gomb);
+            }
+        }
+    }
+}
