6b072a35062fa350b9ef1d410f6534548081ce5c
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-12 15:38:24 +0100

    Számítógép OOP gyakorlás

--- SzamitogepOOP/SzamitogepOOP/Program.cs
+++ SzamitogepOOP/SzamitogepOOP/Program.cs
@@ -10,6 +10,35 @@ namespace SzamitogepOOP
     {
         static void Main(string[] args)
         {
+            Szamitogep gep1 = new Szamitogep(2048, true);
+            Szamitogep gep2 = new Szamitogep();
+            Console.WriteLine(gep1);
+            gep2.Kapcsol();
+            Console.WriteLine(gep2);
+
+            if (gep2.programMasol(800))
+            {
+                Console.WriteLine("Sikeres másolás!");
+                Console.WriteLine(gep2);
+            } else
+            {
+                Console.WriteLine("Sikertelen másolás!");
+                Console.WriteLine(gep2);
+            }
+
+            if (gep2.programMasol(400))
+            {
+                Console.WriteLine("Sikeres másolás!");
+                Console.WriteLine(gep2);
+            }
+            else
+            {
+                Console.WriteLine("Sikertelen másolás!");
+                Console.WriteLine(gep2);
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ SzamitogepOOP/SzamitogepOOP/Szamitogep.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzamitogepOOP
+{
+    public class Szamitogep
+    {
+        private double szabadMemoria;
+        private bool beKapcsolva;
+
+        public Szamitogep()
+        {
+            this.szabadMemoria = 1024;
+            this.beKapcsolva = false;
+        }
+
+        public Szamitogep(double szabadMemoria, bool beKapcsolva)
+        {
+            this.szabadMemoria = szabadMemoria;
+            this.beKapcsolva = beKapcsolva;
+        }
+
+        public void Kapcsol()
+        {
+            beKapcsolva = !beKapcsolva;
+        }
+
+        public bool programMasol(double programMeret)
+        {
+            bool sikeresMasolas = false;
+
+            if (szabadMemoria>programMeret && beKapcsolva)
+            {
+                szabadMemoria -= programMeret;
+                sikeresMasolas = true;
+            }
+
+
+            return sikeresMasolas;
+        }
+
+        public override string ToString()
+        {
+            String adatok = "";
+            if (beKapcsolva)
+            {
+                adatok = adatok + "A számítógép be van kapcsolva";
+            } else
+            {
+                adatok = adatok + "A számítógép ki van kapcsolva";
+            }
+
+            return adatok+$" Szabad memória:{szabadMemoria}";
+        }
+
+    }
+}
