0b4c58b6a83cfa6c695e452f32d3bbbf7e0ae60f
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-11-26 15:19:01 +0100

    Időmérés WPF programban

---
+++ WpfIdomeres/WpfIdomeres/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfIdomeres
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Stopwatch stopper;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            stopper = new Stopwatch();
+        }
+
+        private void buttonStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (stopper.IsRunning)
+            {
+                stopper.Stop();
+                labelIdo.Content = stopper.ElapsedMilliseconds;
+                buttonStartStop.Content = "Start";
+            } else
+            {
+                stopper.Reset();
+                stopper.Start();
+                buttonStartStop.Content = "Stop";
+            }
+              
+            }
+    }
+}
