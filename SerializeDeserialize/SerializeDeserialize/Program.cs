ee52dd224c4c069d2e91261b7bb5eafab459dba0
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-12-18 15:16:30 +0100

    Szerializáció,deszerializáció

---
+++ SerializeDeserialize/SerializeDeserialize/Program.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SerializeDeserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Szemely> szemelyek = new List<Szemely>();
+
+            try
+            {
+                var sorok = File.ReadAllLines(@"tesztadat_20k.txt",Encoding.Default);
+                for (int i = 0; i < sorok.Length; i++)
+                {
+                    szemelyek.Add(new Szemely(sorok[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine(szemelyek.Count);
+
+            //Szerializáció:egy adott objektum fájlba írása
+            //Leggyakrabban XML, vagy JSON formátumba történik
+            //a szerializálás
+
+            var xmlFajl = "szemelyek.xml";
+
+            var serializer = new XmlSerializer(typeof(List<Szemely>));
+
+            using (var fs=new FileStream(xmlFajl,FileMode.Create))
+            {
+                serializer.Serialize(fs, szemelyek);
+            }
+
+            List<Szemely> betolt;
+
+            using (var fs=new FileStream(xmlFajl,FileMode.Open))
+            {
+                betolt = (List<Szemely>)serializer.Deserialize(fs);
+            }
+
+            Console.WriteLine($"A visszatöltött adatok sorai:{betolt.Count}");
+            Console.WriteLine($"Első sor:{betolt[0].Vezeteknev},{betolt[0].Keresztnev}");
+
+            //Szerializálás JSON formátumba
+
+            var jsonSerializer = new JsonSerializer();
+            var jsonFajl = new JsonTextWriter(new StreamWriter(@"szemelyek.json"));
+
+            jsonSerializer.Serialize(jsonFajl, szemelyek);
+            jsonFajl.Close();
+
+            JsonReader jsonBetolt = new JsonTextReader(new StreamReader(@"szemelyek.json"));
+
+            List<Szemely> jsonVissza = jsonSerializer.Deserialize<List<Szemely>>(jsonBetolt);
+
+            jsonBetolt.Close();
+
+            Console.WriteLine(jsonVissza.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SerializeDeserialize/SerializeDeserialize/Szemely.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SerializeDeserialize
+{
+    public class Szemely
+    {
+        
+
+        public string Keresztnev { get; set; }
+        public int SzuletesEve { get; set; }
+        public string SzuletesiHely { get; set; }
+        public string Vezeteknev { get; set; }
+
+        public Szemely()
+        { }
+
+        public Szemely(string sor)
+        {
+            var e=sor.Split(',');
+            Vezeteknev = e[0];
+            Keresztnev = e[1];
+            SzuletesEve = Convert.ToInt32(e[2]);
+            SzuletesiHely = e[3];
+            
+        }
+
+    }
+}
