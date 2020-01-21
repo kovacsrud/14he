e01a21cb0d07444f51c27037f6c4ce2ddd2ec945
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-01-21 15:57:31 +0100

    Adatbázis elérés grafikus felületen

---
+++ WpfSqlite/WpfSqlite/DbRepo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Data;
+using System.Windows.Controls;
+
+namespace WpfSqlite
+{
+    public class DbRepo
+    {
+        string connectionString;
+        DataGrid adatok;
+
+        public DbRepo(string connstring,DataGrid grid)
+        {
+            connectionString = connstring;
+            adatok = grid;
+        }
+
+        public void DbLekerdez()
+        {
+            using (SQLiteConnection conn=new SQLiteConnection(connectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand sqlc=new SQLiteCommand())
+                {
+                    sqlc.Connection = conn;
+                    sqlc.CommandText = "select * from idojarasadatok";
+                    DataTable table = new DataTable();
+                    table.Load(sqlc.ExecuteReader());
+                    adatok.ItemsSource = table.DefaultView;
+                }
+            }
+        }
+
+    }
+
+    
+}
