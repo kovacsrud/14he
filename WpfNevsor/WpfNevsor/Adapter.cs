6fa874c7915a27df1cd50c41da113f3ff12c0b34
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-01-29 17:55:48 +0100

    Névsor adapter osztállyal

---
+++ WpfNevsor/WpfNevsor/Adapter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace WpfNevsor
+{
+    public class Adapter
+    {
+        DataTable table;
+        SQLiteDataAdapter adapter;
+        SQLiteConnection conn;
+
+        public Adapter(string connString)
+        {
+            conn = new SQLiteConnection(connString);
+            table = new DataTable();
+            adapter = new SQLiteDataAdapter("", conn);
+
+            adapter.SelectCommand = new SQLiteCommand(conn);
+            adapter.SelectCommand.CommandText = "select * from nevsor";
+
+            //insert
+            adapter.InsertCommand = new SQLiteCommand(conn);
+            adapter.InsertCommand.CommandText = "insert into nevsor "+
+                "(vezeteknev,keresztnev,szulev,szulhely) "+
+                "values "+
+                "(@vezeteknev,@keresztnev,@szulev,@szulhely)";
+            adapter.InsertCommand.Parameters.Add("@vezeteknev", DbType.String, 0, "vezeteknev");
+            adapter.InsertCommand.Parameters.Add("@keresztnev", DbType.String, 0, "keresztnev");
+            adapter.InsertCommand.Parameters.Add("@szulev", DbType.Int32, 0, "szulev");
+            adapter.InsertCommand.Parameters.Add("@szulhely", DbType.String, 0, "szulhely");
+
+            //update
+            adapter.UpdateCommand = new SQLiteCommand(conn);
+            adapter.UpdateCommand.CommandText = "update nevsor "+
+                "set "+
+                "vezeteknev=@vezeteknev,keresztnev=@keresztnev,szulev=@szulev,szulhely=@szulhely "+
+                "where id=@old_id";
+            adapter.UpdateCommand.Parameters.Add("@vezeteknev", DbType.String, 0, "vezeteknev");
+            adapter.UpdateCommand.Parameters.Add("@keresztnev", DbType.String, 0, "keresztnev");
+            adapter.UpdateCommand.Parameters.Add("@szulev", DbType.Int32, 0, "szulev");
+            adapter.UpdateCommand.Parameters.Add("@szulhely", DbType.String, 0, "szulhely");
+            adapter.UpdateCommand.Parameters.Add("@old_id",DbType.Int32,0,"id").SourceVersion=DataRowVersion.Original;
+
+            //delete
+            adapter.DeleteCommand = new SQLiteCommand(conn);
+            adapter.DeleteCommand.CommandText = "delete from nevsor where id=@old_id";
+            adapter.DeleteCommand.Parameters.Add("@old_id", DbType.Int32, 0, "id").SourceVersion = DataRowVersion.Original;
+
+        }
+
+        public DataTable GetAdatok()
+        {
+            adapter.Fill(table);
+            return table;
+        }
+
+        public void UpdateAdatok()
+        {
+            adapter.Update(table);
+        }
+        
+    }
+}
---
+++ WpfNevsor/WpfNevsor/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfNevsor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Adapter adapter;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            adapter = new Adapter("Data Source=nevsor20k.db;Version=3");
+            nevsorAdatok.ItemsSource = adapter.GetAdatok().DefaultView;
+        }
+
+        private void buttonUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            adapter.UpdateAdatok();
+        }
+    }
+}
